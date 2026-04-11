8ffa0243d8ccd998e86d06e3fd5739b2de9364ff
ChrisIgu <chrismendezaxdl16@gmail.com>
2026-04-10 21:22:39 -0400

    grraawwwwwrrr

--- data/stages/intruder.hx
+++ data/stages/intruder.hx
@@ -1,4 +1,5 @@
 var movecam:Bool = false;
+var totalTime:Float = 0;
 var phase:Int = -1;
 
 var saturation = new FunkinShader('
@@ -18,6 +19,59 @@ var saturation = new FunkinShader('
 
         gl_FragColor = color;
     }
+');
+
+var scroll = new FunkinShader('
+//SHADERTOY PORT FIX
+#pragma header
+vec2 uv = openfl_TextureCoordv.xy;
+vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;
+vec2 iResolution = openfl_TextureSize;
+uniform float iTime;
+#define iChannel0 bitmap
+#define texture flixel_texture2D
+#define fragColor gl_FragColor
+#define mainImage main
+#define time iTime
+//SHADERTOY PORT FIX
+
+// https://www.shadertoy.com/view/WtGGRt
+
+void mainImage()
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    //vec2 uv = fragCoord/iResolution.xy;
+
+    const float timeMulti = 0.2;
+    
+    const float xSpeed = -0.050;
+    const float ySpeed = 0.00;
+
+    
+    float time = iTime * timeMulti;
+    
+    // no floor makes it squiqqly
+    float xCoord = floor(fragCoord.x + time * xSpeed * iResolution.x);
+    float yCoord = floor(fragCoord.y + time * ySpeed * iResolution.y);
+    
+    vec2 coord = vec2(xCoord, yCoord);
+    coord = mod(coord, iResolution.xy);
+ 
+    
+    
+	vec2 uv = coord/iResolution.xy;
+    // Time varying pixel color
+    //vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+    float col = texture(iChannel0, uv).x;
+
+
+    vec3 color = vec3(col);
+    
+    
+    
+    // Output to screen
+    fragColor = vec4(color,1.0) * texture(iChannel0, uv) + texture(iChannel0, uv);
+}
 ');
 
 function postCreate(){
@@ -47,17 +101,24 @@ function postCreate(){
         }
     }
 
-    var coords:Array<Array<Int>> = [ [-100, -220], [370, -220], [1550, -130], [-90, -130], [150, 200], [150, -150], [-50, -350]];
+    var coords:Array<Array<Int>> = [ [-100, -220], [370, -220], [1550, -130], [-90, -130], [150, 200], [150, -150], [-50, -350], [0, 660]];
 
-    for(i in 1...7){
+    for(i in 1...10){
         var sprite = "num" + i;
-        insert(0, sprite = new FlxSprite(coords[i - 1][0], coords[i - 1][1]).loadGraphic(Paths.image("stages/intruderr/" + i)));
-        sprite.scale.set(1.2, 1.2);
+        if(i <= 7){
+            insert(0, sprite = new FunkinSprite(coords[i - 1][0], coords[i - 1][1]).loadGraphic(Paths.image("stages/intruderr/" + i)));
+            if(i == 4) sprite.shader = scroll;
+            if(i == 5) sprite.scrollFactor.set(0.5, 0.8);
+            sprite.scale.set(1.2, 1.2);
+        }else{
+            insert(0, sprite = new FunkinSprite(0, (i == 8) ? 0 : coords[7][1]).makeGraphic(FlxG.width, 60, 0xFF000000));
+            sprite.camera = camHUD;
+        }
     }
 
     for(sprite in [boyfriend, dad]){
         sprite.setPosition(sprite.x + (sprite == boyfriend ? 360 : 340), sprite.y + (sprite == boyfriend ? 265 : 180));
-        sprite.cameraOffset.set((sprite == boyfriend ? -340 : -130), (sprite == boyfriend ? -250 : 100));
+        sprite.cameraOffset.set((sprite == boyfriend ? -340 : 150), (sprite == boyfriend ? -250 : 250));
     }
 
     dad.scale.set(0.75, 0.75);
@@ -124,6 +185,11 @@ function stepHit(){
         FlxTween.tween(boyfriend.cameraOffset, {x: 0, y: 300}, 1.5, {ease: FlxEase.smoothStepInOut});
         FlxTween.tween(FlxG.camera, { zoom: 0.6 }, 2, {ease: FlxEase.smoothStepInOut});
     }
+}
+
+function update(elapsed:Float){
+    totalTime += elapsed;
+    scroll.iTime = totalTime;
 }
 
 function onCameraMove(e:CamMoveEvent){if(movecam){ FlxTween.num(defaultCamZoom, (e.strumLine == strumLines.members[0]) ? 0.8 : 0.55, 0.15, { ease: FlxEase.quadOut }, function(v){ defaultCamZoom = v;});}}
